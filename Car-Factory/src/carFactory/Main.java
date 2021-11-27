0819948ba6dfbb2473a9919854ae5e5576f59e89
mshnwq <mshnwq.warzone@gmail.com>
2021-11-27 19:09:11 +0300

    added request class

--- Car-Factory/src/carFactory/Main.java
+++ Car-Factory/src/carFactory/Main.java
@@ -18,80 +18,81 @@ package carFactory;
 
 	    
 	   public static  void main (String [] args ) 
-
 	   {
+
+
 	       Random random = new Random();
 
-	       
+
 		   String  [] location = { "Riyadh " ,"Makkah" ,"Dammam ","Jeddah"};
 		   int  [] workingHours = {12 ,10 ,16,24};
 	       int storageCapacity;
 	      int  workersCapacity ;
 
-	       
+
 
 
 	     //1A- generate fixed number of warehouses
-	       
-		   ArrayList<Warehouse> wareHouses =  new ArrayList<String>();
+
+		   ArrayList<Warehouse> wareHouses =  new ArrayList<>();
 
 		   for (int i = 0; i<1000; i++)
-			   
-		   {
-			   int Location  = random.nextInt( location.length);               	  
-			   int WorkingHours  = random.nextInt(workingHours.length);                  	  
-			   int storage_Capacity  =  (int)Math.floor(Math.random()*(100000-10000+1)+10000);       ;   
 
-					   
-			  materialQuantity M1 =  new materialQuantity ();                                          
-			   
-			  wareHouses.add(i, new wareHouses( location[Location] , workingHours[WorkingHours] , M1 , storageCapacity =storage_Capacity ));
+		   {
+			   int Location  = random.nextInt( location.length);
+			   int WorkingHours  = random.nextInt(workingHours.length);
+			   int storage_Capacity  =  (int)Math.floor(Math.random()*(100000-10000+1)+10000);       ;
+
+
+			  materialQuantity M1 =  new materialQuantity ();
+
+				wareHouses.add(i, new WareHouse( location[Location] , workingHours[WorkingHours] , M1 , storageCapacity =storage_Capacity ));
 
 	 	 }
-		   
-		   
+
+
 		   	//1B- generate fixed number of factories
 
 		   ArrayList<Factory> factories=  new ArrayList<String>();
-		   
+
 		   for (int i = 0; i<100; i++)
-			   
+
 		   {
-			   
-			   int Location  = random.nextInt( location.length);               	  
-			   int WorkingHours  = random.nextInt(workingHours.length);   
-			   int workers_Capacity  =  (int)Math.floor(Math.random()*(2000-500+1)+500);       ; 
+
+			   int Location  = random.nextInt( location.length);
+			   int WorkingHours  = random.nextInt(workingHours.length);
+			   int workers_Capacity  =  (int)Math.floor(Math.random()*(2000-500+1)+500);       ;
 
 
-			   
-			   factories.add(i, new factories( location[Location] , workingHours[WorkingHours] , wareHouses , workersCapacity=workers_Capacity  ));
-	    	
+
+			  ï¿½factories.add(i, new factories( location[Location] , workingHours[WorkingHours] , wareHouses , workersCapacity=workers_Capacity  ));
+
 	 	 }
-		   
-			
-		   
-			    
-	  	/*------Loop-------*/
-		   
-			public ArrayList<Request> requests = new ArrayList<> ; 
 
-		   while (true) 
+
+
+
+	  	/*------Loop-------*/
+
+			public ArrayList<Request> requests = new ArrayList<> ;
+
+		   while (true)
 		   {
 			   //3- generate requests
 
-			       requests.add(i, new requests(selectedComputers,selectedCost,selectedTime);
+			   ï¿½ï¿½ï¿½ï¿½requests.add(i, new requests(selectedComputers,selectedCost,selectedTime);
 
-			   
+
 			   //4- fulfill requests
 			   requests.findFactory(ArrayList<Factory>);
-			   
+
 		    	//2-simulate current days counter
 
 		   }
-		   
-	 
+
+
 	    	///5- output report
-		   
+
 	    	///+toString(): String
 	   }
 	   
---
+++ Car-Factory/src/carFactory/Request.java
@@ -0,0 +1,4 @@
+package carFactory;
+
+public class Request {
+}
