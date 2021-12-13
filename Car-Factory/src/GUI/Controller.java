1298074abad7c715ee3d6c4b4416e21471950318
Faisal'git config --global user.email faisal-2052@hotmail.comgit config --global user.name "Faisal <faisal-2052@hotmail.com'git config --global user.name Faisalgit config --global user.email "faisal-2052@hotmail.com>
2021-12-13 21:07:38 +0300

    Phase 1 GUI

--- Car-Factory/src/GUI/Controller.java
+++ Car-Factory/src/GUI/Controller.java
@@ -1,25 +1,34 @@
 package GUI;
 
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
 
 public class Controller {
+	private Stage stage;
+	private Scene scene;
+	private Parent root;
 	
-	@FXML
-	private Circle myCircle;
-	private double x;
-	private double y;
-	public void up(ActionEvent e) {
-		myCircle.setCenterY(y -= 10);
+	public void switchToPhase1(ActionEvent event) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("Phase1.fxml"));
+		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
 	}
-	public void left(ActionEvent e) {
-		myCircle.setCenterX(x -= 10);
+	
+	public void switchToPhase2(ActionEvent event) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("Phase2.fxml"));
+		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
 	}
-	public void down(ActionEvent e) {
-		myCircle.setCenterY(y += 10);
-	}
-	public void right(ActionEvent e) {
-		myCircle.setCenterX(x += 10);
-	}
+	
 }
--- Car-Factory/src/GUI/Main.java
+++ Car-Factory/src/GUI/Main.java
@@ -1,26 +1,33 @@
 package GUI;
 
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
 
 
 public class Main extends Application {
-	@Override
-	public void start(Stage Phase1) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			Phase1.setScene(scene);
-			Phase1.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
 	
 	public static void main(String[] args) {
 		launch(args);
 	}
+	@Override
+	public void start(Stage stage) throws Exception {
+		
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("Phase1.fxml"));
+			Scene scene = new Scene(root);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			stage.getIcons().add(new Image("icon.png"));
+			stage.setTitle("Computer Factory Simulator");
+			stage.setScene(scene);
+			stage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		
+	}  
 }
